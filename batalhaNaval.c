fbeea032bf3d2ec969126e9842c869277dd0d6a3
Gustavo Ternovoe <headshotbrter@gmail.com>
2026-03-23 18:30:48 -0300

    Desafio Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+void navios(int matriz[][10]){ //FUNCAO PARA CRIAR NAVIOS, FUNCAO RECEBE MATRIZ (PS É NECESSARIO COLOCAR O TAMANHO DE [][Y] NO PARAMETRO.
+  
+  int navio1[3] = {3, 3, 3}; //VETOR NAVIO 1
+  int navio2[3] = {3, 3, 3}; //VETOR NAVIO 2
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+  
+    for (int y = 0; y < 3; y++){
+ 
+         matriz[2][3 + y] = navio1[y];   //Posiciona o vetor navio dentro da matriz a partir de [2]x Linha
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    for (int y = 0; y < 3; y++){
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+         matriz[4 + y][2] = navio2[y]; //Posiciona o vetor navio dentro da matriz a partir de [2]y coluna
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    }
+  
+  
 
-    return 0;
 }
+
+void tabuleiro(){
+     
+  int tabuleiro[10][10]; // Matriz 10x10;
+
+//=====================================================
+//         LOOP PARA PRENCHER A MATRIZ COM ZEROS E POSICIONAR NAVIOS ;
+//=====================================================
+
+      for (int x = 0; x < 10; x++){
+         
+         for (int y = 0; y < 10; y++){
+         
+            tabuleiro[x][y] = 0;
+         }
+      }
+
+      navios(tabuleiro); //FUNCAO QUE CRIA OS NAVIOS E POSICIONA DENTRO DO TABULEIRO;
+
+      //tabuleiro[2][3] = 3;
+
+      //navios(tabuleiro[4][5], tabuleiro[4][5]);
+
+
+//=====================================================
+//         LOOP PARA PRINTAR O TABULEIRO : 
+//=====================================================
+  
+ printf("\n\n\n\n\n\n\n ---- TABULEIRO BATALHA NAVAL ----\n");
+ printf("\n   A  B  C  D  E  F  J  K  L  J \n");
+
+  for (int x = 0; x < 10; x++){ // lOOP DE X
+   
+
+   //PRINTA OS NUMEROS LATERAIS DO TABULEIRO; OPERADOR TERNARIO ESTA SENDO USADO PARA TIRAR O ESPACAMENTO ENTRE O 10 E OS 0 :
+   ((x + 1) < 10) ? printf("%d ", x + 1) : printf("%d", x + 1); 
+
+
+    for (int y = 0; y < 10; y++){ // lOOP DE Y
+     
+   // PRINT DO VALOR EM [Y] DENTRO DE [X] OU SEJA : [X][Y] = "VALOR";
+      printf(" %d ", tabuleiro[x][y]); 
+
+    }
+
+   printf("\n"); //QUEBRA LINHA A CADA VETOR[X]
+
+  }
+  printf("\n\n\n\n"); //LIBERAR ESPACO NO TERMINAL (AJUSTE VISUAL);
+
+}
+
+
+
+int main(){
+ 
+  tabuleiro();
+
+ return 0;
+
+}
+
